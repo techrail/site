8ebc55737d68d1cdc02a073aacf02fe60422f2e9
Vaibhav Kaushal <vaibhav-kaushal@users.noreply.github.com>
2025-02-21 09:50:45 +0530

    Update quartz.config.ts

--- private/quartz.config.ts
+++ private/quartz.config.ts
@@ -49,7 +49,13 @@ const config: QuartzConfig = {
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "filesystem"], // you can add 'git' here for last modified from Git but this makes the build slower
       }),
-      Plugin.SyntaxHighlighting(),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
